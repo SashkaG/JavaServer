90d637e233ae50badf46e82685099a311557b32e
SashkaG <4807091256@mail.ru>
2017-07-18 22:27:37 +0300

    heroku deploy

---
+++ src/main/java/Server.java
@@ -0,0 +1,101 @@
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    public static void main(String[] args) {
+
+        Server serv = new Server();
+    }
+    class AceptedThread extends Thread
+    {
+        Server serv;
+        ServerSocket serverSocket = null;
+        AceptedThread(Server main,ServerSocket main2)
+        {
+            this.serv=main;
+            this.serverSocket=main2;
+        }
+        public void run()
+        {
+            while (true) {
+                try {
+                    Socket socket = null;
+                    socket = serverSocket.accept();
+                    ConnectionThread a = new ConnectionThread(socket,serv);
+                    serv.conns.add(a);
+                    a.start();
+                } catch (IOException e) {
+                    System.out.println("I/O error: " + e);
+                }
+                // new thread for a client
+
+            }
+        }
+    }
+    ArrayList<ConnectionThread> conns = new ArrayList<>();
+    public Server()
+    {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4040);
+            new AceptedThread(this,serverSocket).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+}
+class ConnectionThread extends Thread{
+    private Socket socket;
+    private Server server;
+    private PrintWriter out;
+    ConnectionThread(Socket sock, Server serv)
+    {
+        this.server=serv;
+        this.socket=sock;
+    }
+    public void send(String message)
+    {
+        if(out!=null)
+        {
+            out.println(message);
+        }
+    }
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        out = null;
+        try {
+            inp = socket.getInputStream();
+            brinp = new BufferedReader(new InputStreamReader(inp));
+            out = new PrintWriter(socket.getOutputStream(),true);
+        } catch (IOException e) {
+            return;
+        }
+        String line;
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("FINISH")) {
+                    socket.close();
+                    return;
+                } else {
+                    for(ConnectionThread e : server.conns)
+                    {
+                        e.send(line);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
+
+
